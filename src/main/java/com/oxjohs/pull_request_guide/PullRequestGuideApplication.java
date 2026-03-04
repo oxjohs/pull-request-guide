9a8c64befa317b00913b807f685df68d1b928b40
Hyunsung Jo <oxjohs@gmail.com>
2026-03-04 21:17:21 +0900

    chore: initial bootstrap

---
+++ src/main/java/com/oxjohs/pull_request_guide/PullRequestGuideApplication.java
@@ -0,0 +1,13 @@
+package com.oxjohs.pull_request_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PullRequestGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oxjohs/pull_request_guide/PullRequestGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxjohs.pull_request_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
